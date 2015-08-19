3a669e44cefcb05220e6ec923c2be59e7ee220c8
Katie <katie.k7r@gmail.com>
2015-08-19 19:18:33 -0400

    Adding typicons, updating slim views, and messing around with stylesheets a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # Helper for easier construction of typicon icons with screenreader text
+  def typicon(icon_name, screenreader_text)
+    typcn  = "<span aria-hidden='true' class='typcn typcn-#{icon_name}'></span>"
+    typcn += "<span class='sr'>#{screenreader_text}</span>"
+    return typcn.html_safe
+  end
 end
